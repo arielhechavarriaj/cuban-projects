6fce788c99a8bcb7cd8708a8172960b72f88cda6
leiraStudio <leiraStudio@gmail.com>
2022-02-11 13:30:54 -0500

    admin panel 1.0.0

---
+++ projects/api-ui/src/app/shared/cubanApi.service.service.ts
@@ -0,0 +1,65 @@
+
+
+import { HttpClient, HttpParams } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import { map, Observable } from 'rxjs';
+import { Project, ProjectscubanOpenSource } from 'src/assets/projects';
+
+
+@Injectable({
+  providedIn: 'root',
+})
+export class CubanAPIService {
+  apiUrl = 'http://localhost:3000/api/v1/';
+  apiURLProjects = 'http://localhost:3000/api/v1/' + 'projects';
+  constructor(private http: HttpClient) {}
+  getCategories(): Observable<any[]> {
+    return this.http.get<any[]>('http://localhost:3000/api/v1/categories');
+  }
+
+  getProjectsByCategory(categoryId: string): Observable<any[]> {
+    return this.http.get<any[]>(
+      `http://localhost:3000/api/v1/projectsByCategory/${categoryId}`
+    );
+  }
+
+  getProjects(categoriesFilter?: string[]): Observable<Project[]> {
+    let params = new HttpParams();
+    if (categoriesFilter) {
+      params = params.append('categories', categoriesFilter.join(','));
+    }
+    return this.http.get<Project[]>(this.apiURLProjects, { params: params });
+  }
+
+  createProject(projectData: FormData): Observable<Project> {
+    return this.http.post<Project>(this.apiURLProjects, projectData);
+  }
+
+  getProject(projectId: string): Observable<Project> {
+    return this.http.get<Project>(`${this.apiURLProjects}/${projectId}`);
+  }
+
+  updateProject( projectid: any,projectData: FormData): Observable<Project> {
+    return this.http.put<Project>(
+      `${this.apiURLProjects}/${projectid}`,
+      projectData
+    );
+  }
+
+  deleteProject(projectId: string): Observable<any> {
+    return this.http.delete<any>(`${this.apiURLProjects}/${projectId}`);
+  }
+
+  getProjectCount(): Observable<number> {
+    return this.http
+      .get<number>(`${this.apiURLProjects}/get/count`)
+      .pipe(map((objectValue: any) => objectValue.projectCount));
+  }
+
+  getProjectByID(projectid: string | null): Observable<Project> {
+    return this.http.get<Project>(
+      `${this.apiURLProjects}/${projectid}`,
+    );
+  }
+
+}
---
+++ projects/api-ui/src/app/shared/interfaces.ts
@@ -0,0 +1,19 @@
+export interface ProjectscubanOpenSource {
+  "name": string;
+  "url": string;
+  "description": string;
+  id: string;
+}
+export interface category {
+  category: string;
+  "description": string;
+  "name"?: string;
+  icon?: string;
+  id:"",
+}
+export interface Project {
+  id: string;
+  "name": string;
+  "url": string;
+  "description": string;
+}
