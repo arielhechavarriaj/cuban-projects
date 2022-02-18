aeef7d85e3ac4d78adce2037776d428681bea99e
leiraStudio <leiraStudio@gmail.com>
2022-02-18 03:34:35 -0500

    deploy heroku

--- server.js
+++ server.js
@@ -5,4 +5,4 @@ app.use(express.static(__dirname + '/dist/<cuban-open-source>'));
 app.get('/*', function(req,res) {
     res.sendFile(path.join(__dirname+
         '/dist/<cuban-open-source>/index.html'));});
-app.listen(process.env.PORT || 8080);
+app.listen(8080);
