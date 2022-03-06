562388a430e3885cb153456f72607fd525aaaee2
Ariel hechavarria <leiraStudio@gmail.com>
2022-03-06 18:54:35 -0500

    Update server.js

--- server.js
+++ server.js
@@ -4,6 +4,6 @@ const app = express();
 app.use(express.static(__dirname + '/dist/<cuban-open-source>'));
 app.get('/*', function(req,res) {
     res.sendFile(path.join(__dirname+
-        '/dist/<cuban-open-source>/index.html'));});
+        '/dist/cuban-open-source/index.html'));});
 //server listening
 app.listen(process.env.PORT || 5000)
